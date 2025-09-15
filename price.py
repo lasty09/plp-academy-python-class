a39a35be45de96888ebb97b7871796b12e31762f
lasty09 <freddiemuhoho1@gmail.com>
2025-09-15 21:45:10 +0300

    finished all assignment

--- price.py
+++ price.py
@@ -6,3 +6,19 @@ def calculate_discount(price, discount_percent):
         return total_price
     else:
         return price
+
+
+
+
+##quiz 2
+def calculate_discount(price, discount_percent):
+    
+    if discount_percent >= 20:
+        discount_amount = price * (discount_percent / 100)
+        total_price = price - discount_amount
+        return total_price
+    else:
+        return price
+
+print("🛍️  Discount Calculator")
+print("=" * 30)
